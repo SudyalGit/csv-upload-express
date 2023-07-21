ce887fe0ff3d11443e07641f65b18f25b39d4ad9
Sudhanshu <sudyal101@gmail.com>
2023-07-21 08:18:35 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+// Importing packages
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+// Importing controllers
+const home_controller = require('../controllers/home_controller');
+const file_controller = require('../controllers/file_controller');
+const uploadFile = multer({ dest: 'uploads/files' }).single('file');
+
+// Router
+router.get('/', home_controller.home);
+router.post('/upload', uploadFile, file_controller.upload);
+router.get('/view/:id', file_controller.view);
+router.get('/delete/:id', file_controller.delete);
+
+module.exports = router;
